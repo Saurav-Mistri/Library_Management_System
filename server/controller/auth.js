1761ebd25ab155710ff84a0b42091b28964f7a88
Saurav-Mistri <sauravmistri21@gmail.com>
2025-03-28 23:46:30 +0530

    login logic code is done

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 /** 
  * @route POST v1/auth/register
@@ -38,6 +39,53 @@ export async function Register(req, res) {
             status: "success",
             data: { user_data },
             message: "Thank you for registering with us. Your account has been successfully registered."
+        });
+    } catch (err) {
+        res.status(500).json({
+            status: "error",
+            code: 500,
+            data: [],
+            message: err,
+        });
+    }
+    res.end();
+}
+
+// Login login code 
+
+export async function Login(req, res) {
+    // Get variables for the login process
+    const { email } = req.body;
+
+    try {
+        // Verify if the user is exists
+        const user = await User.findOne({ email }).select("+password");
+        if (!user) {
+            return res.status(401).json({
+                status: "failed",
+                data: [],
+                message: "Invalid email or password! Please try again with the correct/valid credentials!",
+            });
+        }
+        // if user exists
+        // Step - 1 validate password
+        const isPasswordValid = await bcrypt.password(
+            `$(req.body.password)`,
+            user.password
+        );
+        if (!isPasswordValid) {
+            return res.status(401).json({
+                status: "failed",
+                data: [],
+                message: "Invalid email or password! Please try again with the correct/valid credentials",
+            });
+        }
+        const { password, ...user_data } = user._doc;
+
+        res.status(200).json({
+            status: "success",
+            data: [user_data],
+            message: "Your have successfully logged-in!",
         });
     } catch (err) {
         res.status(500).json({
