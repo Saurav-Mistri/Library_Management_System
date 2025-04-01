4055cddd1808570c0cec84336cb887fd5d4e354c
Saurav-Mistri <sauravmistri21@gmail.com>
2025-04-01 21:58:42 +0530

    login and registration file basic code is done

---
+++ client/src/auth/auth.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function auth() {
+  return (
+    <div>
+      LOGIN Page
+    </div>
+  )
+}
+
+export default auth
---
+++ client/src/auth/register.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function register() {
+  return (
+    <div>
+      Registration Page
+    </div>
+  )
+}
+
+export default register
