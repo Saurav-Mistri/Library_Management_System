063fcce1f3a6ad86ef53439d5aba64a947d2e059
Saurav-Mistri <sauravmistri21@gmail.com>
2025-04-01 22:17:50 +0530

    basic users file code is done

---
+++ client/src/admin/users/UserCreate.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserCreate() {
+    return (
+        <div>
+            Create User
+        </div>
+    )
+}
+
+export default UserCreate
---
+++ client/src/admin/users/UserEdit.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserEdit() {
+    return (
+        <div>
+            Edit User
+        </div>
+    )
+}
+
+export default UserEdit
